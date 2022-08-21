735f0019a182a090e126c9d0557e4194312aaff7
Simon Bundgaard-Egeberg <seg@it-minds.dk>
2022-08-21 09:54:40 +0200

    dev container

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,3 +9,20 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+
+alias SummerUni.{Accouts, Posts}
+
+users =
+  ~w(Rosalyn Blake Sasha Phyllis Clarissa Trevino Felicia Cora Melissa Violet Brown Lessie Taylor Sherri Margeret Kristy Noemi Trisha Hendrix Mann)
+
+inserted_users =
+  users
+  |> Enum.map(fn user ->
+    {:ok, user} = Accouts.register_user(%{email: "#{user}@email.com", password: "password1234"})
+    user
+  end)
+
+inserted_users
+|> Enum.each(fn user ->
+  Posts.create_post(%{text: "I am the best, fuck the rest", likes: 0}, user)
+end)
